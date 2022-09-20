7d638558d79f44c563b1f0540d32de68822a7956
artemvin007 <78811396+artemvin007@users.noreply.github.com>
2022-09-20 14:15:02 +0300

    Create Main.java

---
+++ src/HomeWork1/task6/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static boolean checkNegative(int a){
+        return (a < 0);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(checkNegative(5));
+        System.out.println(checkNegative(-7));
+    }
+}
