90f4bb5fcd74375ac423b0675290a85170bad704
artemvin007 <78811396+artemvin007@users.noreply.github.com>
2022-09-20 13:59:47 +0300

    Create Main.java

---
+++ src/HomeWork1/task3/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static double computing(int a, int b, int c, int d){
+        return (a * (b + (c / d)));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(computing(1, 2, 3, 4));
+    }
+}
