1edf672e4f4144ff01cf9e02e76360599acf7dbd
artemvin007 <78811396+artemvin007@users.noreply.github.com>
2022-09-20 13:51:27 +0300

    Create Main.java

---
+++ src/HomeWork1/task2/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        byte b = 0;
+        short s = 0;
+        int a = 0;
+        long l = 922337;
+        float f = 1.1f;
+        double d = 15.5;
+        boolean bool = true;
+        char c = 'c';
+    }
+}
