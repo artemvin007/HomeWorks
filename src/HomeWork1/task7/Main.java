263ef95278f246a85339ad207ba13abdd36bd293
artemvin007 <78811396+artemvin007@users.noreply.github.com>
2022-09-20 14:18:10 +0300

    Create Main.java

---
+++ src/HomeWork1/task7/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void hello(String name){
+        System.out.println("Hello " + name + "!");
+    }
+
+    public static void main(String[] args) {
+        hello("Alexandr");
+    }
+}
